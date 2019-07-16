a1503276e144c1c3095b881db1c0225e8f143f6d
MesopotamiazZ <Tigris_wzd@163.com>
2019-07-16 17:20:16 +0800

    新增vuex热更替功能

--- client/store/store.js
+++ client/store/store.js
@@ -8,12 +8,36 @@ import actions from './actions/actions'
 // Vue.use(Vuex)
 
 export default () => {
-  return new Vuex.Store({
+  const store = new Vuex.Store({
     state: defaultState,
     mutations,
     getters,
-    actions
+    actions,
+    modules: {
+    }
   })
+
+  if (module.hot) {
+    module.hot.accept([
+      './state/state',
+      './mutations/mutations',
+      './getters/getters',
+      './actions/actions'
+    ], () => {
+      const newState = require('./state/state').default
+      const newMutations = require('./mutations/mutations').default
+      const newActions = require('./actions/actions').default
+      const newGetters = require('./getters/getters').default
+
+      store.hotUpdate({
+        state: newState,
+        mutations: newMutations,
+        getters: newGetters,
+        actions: newActions
+      })
+    })
+  }
+  return store
 }
 // const store = new Vuex.Store({
 //   state: {
